d8e3ebbe48e70af42e743f4e48bffad28f7e1f5f
maicol abbiento <128224384+MaicolAbbiento@users.noreply.github.com>
2023-08-21 19:00:22 +0200

    ok

--- src/app/services/strive-api.service.ts
+++ src/app/services/strive-api.service.ts
@@ -4,29 +4,22 @@ import { IProfile, IUpdateProfile } from 'src/app/interfaces/iprofile';
 //Token
 import { Token } from 'src/app/environment/token';
 //Http
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 //Tap
 import { Observable, retry, tap } from 'rxjs';
 
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StriveApiService {
+  readonly url: string = 'https://striveschool-api.herokuapp.com/api/profile';
+  token: string = '';
 
-  readonly url:string = "https://striveschool-api.herokuapp.com/api/profile"
-  token:string = ""
-
-  constructor
-  (
-    private http: HttpClient
-  )
-  {
-    this.token = new Token().token
-    this.getUsers()
-    this.getUser("609a57ecdfccc50015a6bbb8")
-    this.getProfile()
+  constructor(private http: HttpClient) {
+    this.token = new Token().token;
+    this.getUsers();
+    this.getUser('609a57ecdfccc50015a6bbb8');
+    this.getProfile();
     /*
     this.setUser({  name: "Alessio2",
                     surname   : "Portacci2",
@@ -38,37 +31,27 @@ export class StriveApiService {
     */
   }
 
-  getUser(userId:string): Observable<IProfile>
-  {
-    return this.http.get<IProfile>(`${this.url}/${userId}`,
-      { headers: {Authorization: `Bearer ${this.token}`} })
+  getUser(userId: string): Observable<IProfile> {
+    return this.http.get<IProfile>(`${this.url}/${userId}`, {
+      headers: { Authorization: `Bearer ${this.token}` },
+    });
   }
 
-  getUsers(): Observable<IProfile[]>
-  {
-    return this.http.get<IProfile[]>
-    (
-      this.url,
-      { headers: {Authorization:  `Bearer ${this.token}`}}
-    )
+  getUsers(): Observable<IProfile[]> {
+    return this.http.get<IProfile[]>(this.url, {
+      headers: { Authorization: `Bearer ${this.token}` },
+    });
   }
 
-  getProfile(): Observable<IProfile>
-  {
-    return this.http.get<IProfile>
-    (
-      `${this.url}/me`,
-      { headers: {Authorization:  `Bearer ${this.token}`}}
-    )
+  getProfile(): Observable<IProfile> {
+    return this.http.get<IProfile>(`${this.url}/me`, {
+      headers: { Authorization: `Bearer ${this.token}` },
+    });
   }
 
-  setUser(userUpdate: IUpdateProfile): Observable<IProfile>
-  {
-    return this.http.put<IProfile>
-    (
-      `${this.url}`,
-      userUpdate,
-      { headers: {Authorization:  `Bearer ${this.token}`}}
-    )
+  setUser(userUpdate: IUpdateProfile): Observable<IProfile> {
+    return this.http.put<IProfile>(`${this.url}`, userUpdate, {
+      headers: { Authorization: `Bearer ${this.token}` },
+    });
   }
 }
