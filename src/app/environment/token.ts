86057ccc5a54785f097b423db0589d9c4dbef99e
alessioportacci <alessio.portacci.s@gmail.com>
2023-08-22 16:39:49 +0200

    Rimosso token

--- src/app/environment/token.ts
+++
@@ -1,6 +0,0 @@
-export class Token {
-  public token: string = '';
-  constructor() {
-    this.token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMTQzNjFmMTc1YzAwMTRjNTU4YzIiLCJpYXQiOjE2OTI2MDM0NTIsImV4cCI6MTY5MzgxMzA1Mn0.WXQGk40aQNcnRXERDD0eiorjA06my8NUQwkdRAA9LXU'  }
-}
