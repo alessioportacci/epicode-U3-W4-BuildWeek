c4cf587695826aafcf3303b6537355d3f1b69b83
GurinderMann <manngurinder110@gmail.com>
2023-08-24 10:40:56 +0200

    add new expirence function

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -3,8 +3,10 @@ import { ExperienceService } from './../../services/experience.service';
 import { StriveApiService } from 'src/app/services/strive-api.service';
 import { IProfile } from './../../interfaces/iprofile';
 
+
 import { Component, OnInit } from '@angular/core';
-import { Iexperiences } from 'src/app/interfaces/iexperiences';
+import { IUpdateExperience, Iexperiences } from 'src/app/interfaces/iexperiences';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -15,10 +17,20 @@ export class ProfileComponent implements OnInit {
   profileData?: IProfile;
   experienceData?: Iexperiences[]
 
+  role: string = '';
+  company:string='';
+  startDate:string='';
+  endDate:string='';
+  description:string='';
+  area:string='';
+
+  formattedEndDate: string = '';
+
   constructor
   (
     public striveSrv: StriveApiService,
-    public experienceSrv: ExperienceService
+    public experienceSrv: ExperienceService,
+
   ) {}
 
   ngOnInit(): void
@@ -37,7 +49,39 @@ export class ProfileComponent implements OnInit {
 
      })
 
+
   }
+
+
+  onSubmit(form: NgForm): void {
+    if (form && form.valid) {
+      console.log(form.value);
+      const experienceData = {
+            role: form.value.role,
+            company: form.value.company,
+            startDate: form.value.startDate,
+            endDate: form.value.endDate,
+            description: form.value.description,
+            area: form.value.area,
+      };
+
+      this.submitExperience(experienceData);
+
+
+      form.reset();
+    }
+  }
+
+
+  submitExperience(experience: any): void {
+    this.experienceSrv.setExperience(experience).subscribe((response) => {
+      console.log('Esperienza aggiunta:', response);
+      this.experienceSrv.getExperiences().subscribe(((data) =>
+      {
+        this.experienceData = data
+        console.log('esperienza', data)
+      }))
+  })}
 
   getProfileData() {}
 }
