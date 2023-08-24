ad54c9d1238ffd75809bf6cb8fe322e32888b731
GurinderMann <manngurinder110@gmail.com>
2023-08-24 16:51:33 +0200

    Homepage v1.0

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,6 +5,8 @@ import { Inews } from 'src/app/interfaces/inews';
 import { CommentsService } from 'src/app/services/comments.service';
 import { NewsService } from 'src/app/services/news.service';
 import { Icomments } from 'src/app/interfaces/icomments';
+import { StriveApiService } from 'src/app/services/strive-api.service';
+import { IProfile } from 'src/app/interfaces/iprofile';
 
 @Component({
   selector: 'app-home',
@@ -15,12 +17,14 @@ export class HomeComponent implements OnInit
 {
   posts: Inews[] = []
   comments: Icomments [] = []
+  profileData?: IProfile;
 
 
   constructor
   (
     private postSrv: NewsService,
-    private commentsSrv: CommentsService
+    private commentsSrv: CommentsService,
+    public striveSrv: StriveApiService
   )
   {
 
@@ -37,6 +41,14 @@ export class HomeComponent implements OnInit
 
       this.commentsSrv.getAllComments().subscribe(res => this.comments = res)
     this.postSrv.getPost("64e7111fad24970014693641").subscribe(res => console.log(res))
+
+
+    this.striveSrv.getProfile().subscribe((data) => {
+      console.log(data);
+      this.profileData = data;
+
+
+    });
   }
 
   loadComments(postId: string) :void
