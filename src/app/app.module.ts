ecf8fe248be4126fe0e647c885593298ebc8ff57
Alessio <alessio.portacci.s@gmail.com>
2023-08-24 17:29:57 +0200

    Rimozione dall'app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule } from '@angular/forms';
-import { ProfiliComponent } from './components/profili/profili.component';
 import { EsperienzeComponent } from './pages/esperienze/esperienze.component';
 import { DateFormatterPipe } from './pipes/date-formatter.pipe';
 import { ModificaEsperienzeComponent } from './pages/modifica-esperienze/modifica-esperienze.component';
@@ -22,7 +21,6 @@ import { DateDayPipe } from './pipes/date-day.pipe';
     ProfileComponent,
     NavbarComponent,
     FooterComponent,
-    ProfiliComponent,
     EsperienzeComponent,
     DateFormatterPipe,
     ModificaEsperienzeComponent,
