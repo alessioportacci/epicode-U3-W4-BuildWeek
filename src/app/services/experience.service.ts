712ada7e0089cb35213d7a38f3b7b631396db89c
maicol abbiento <128224384+MaicolAbbiento@users.noreply.github.com>
2023-08-24 12:18:59 +0200

    ok

--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -5,73 +5,75 @@ import { HttpClient } from '@angular/common/http';
 import { IUpdateExperience, Iexperiences } from '../interfaces/iexperiences';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ExperienceService
-{
+export class ExperienceService {
+  userId: string = '';
 
-  userId :string= ""
+  readonly url: string = `https://striveschool-api.herokuapp.com/api/profile/${this.userId}/experiences`;
+  token: string = new Token().token;
 
-  readonly url:string = `https://striveschool-api.herokuapp.com/api/profile/${this.userId}/experiences`
-  token:string = new Token().token
+  constructor(private http: HttpClient) {}
 
-  constructor
-  (
-    private http: HttpClient,
-  )
-  {
-
+  setUserId(id: string) {
+    this.userId = id;
   }
 
-  setUserId(id: string)
-  {
-    this.userId = id
+  getExperience(): Observable<Iexperiences> {
+    return this.http.get<Iexperiences>(
+      `https://striveschool-api.herokuapp.com/api/profile/${this.userId}/experiences`,
+      {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+        },
+      }
+    );
   }
 
-  getExperience(): Observable<Iexperiences>
-  {
-    return this.http.get<Iexperiences> ( `https://striveschool-api.herokuapp.com/api/profile/${this.userId}/experiences`,
-      { headers: {Authorization:  `Bearer ${this.token}`}}
-    )
+  getExperiences(): Observable<Iexperiences[]> {
+    console.log('userid ', this.userId);
+    return this.http.get<Iexperiences[]>(
+      `https://striveschool-api.herokuapp.com/api/profile/${this.userId}/experiences`,
+      {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+          responseType: 'text',
+        },
+      }
+    );
   }
 
-  getExperiences(): Observable<Iexperiences[]>
-  {
-    console.log('userid ',this.userId)
-    return this.http.get<Iexperiences[]>(`https://striveschool-api.herokuapp.com/api/profile/${this.userId}/experiences`,
-            { headers: {Authorization: `Bearer ${this.token}`} })
-  }
-
-  updateExperience(experienceId: string, experience: IUpdateExperience): Observable<IUpdateExperience>
-  {
-    return this.http.put<Iexperiences>
-    (
+  updateExperience(
+    experienceId: string,
+    experience: IUpdateExperience
+  ): Observable<IUpdateExperience> {
+    return this.http.put<Iexperiences>(
       `${this.url}/${experienceId}`,
       experience,
-      { headers: {Authorization:  `Bearer ${this.token}`}}
-    )
+      { headers: { Authorization: `Bearer ${this.token}` } }
+    );
   }
 
-  setExperience(experience: IUpdateExperience): Observable<IUpdateExperience>
-  {
-    console.log(experience)
-    return this.http.post<Iexperiences>
-    (
-      `https://striveschool-api.herokuapp.com/api/profile/${this.userId}/experiences`,
-      experience,
-      { headers: {Authorization:  `Bearer ${this.token}`}}
-
-    ).pipe(tap(res => console.log(res)))
+  setExperience(experience: IUpdateExperience): Observable<IUpdateExperience> {
+    console.log(experience);
+    return this.http
+      .post<Iexperiences>(
+        `https://striveschool-api.herokuapp.com/api/profile/${this.userId}/experiences`,
+        experience,
+        { headers: { Authorization: `Bearer ${this.token}` } }
+      )
+      .pipe(tap((res) => console.log(res)));
   }
 
-  removeExperience(experienceId: string): Observable<IUpdateExperience>
-  {
-    return this.http.delete<Iexperiences>
-    (
+  removeExperience(experienceId: string): Observable<IUpdateExperience> {
+    return this.http.delete<Iexperiences>(
       `https://striveschool-api.herokuapp.com/api/profile/${this.userId}/experiences/${experienceId}`,
-      { headers: {Authorization:  `Bearer ${this.token}`}}
-    )
+      {
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+          responseType: 'text',
+        },
+      }
+    );
   }
-
 }
-
