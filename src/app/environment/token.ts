f60fc464a852f15fbcef8be6c29d2faa177c254c
maicol abbiento <128224384+MaicolAbbiento@users.noreply.github.com>
2023-08-24 15:27:20 +0200

    Update token.ts

--- src/app/environment/token.ts
+++ src/app/environment/token.ts
@@ -4,8 +4,12 @@ export class Token {
   constructor()
   {
     this.token =
+<<<<<<< Updated upstream
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMTQzNjFmMTc1YzAwMTRjNTU4YzIiLCJpYXQiOjE2OTI2MDM0NTIsImV4cCI6MTY5MzgxMzA1Mn0.WXQGk40aQNcnRXERDD0eiorjA06my8NUQwkdRAA9LXU';
     this.tokenCommenti =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU3MWQ3Y2FkMjQ5NzAwMTQ2OTM2NjMiLCJpYXQiOjE2OTI4Njc5NjUsImV4cCI6MTY5NDA3NzU2NX0.lttDMYVpXNMwL7WDO-GIB2O83kYrf4UDPxp4_aowK1A'
+=======
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU0YWYyNmRmZmI4YjAwMTQ0MTNjMjciLCJpYXQiOjE2OTI3MDg2NzgsImV4cCI6MTY5MzkxODI3OH0.yiC9L7uX3_lRcmxl-IfoErVt8u5G651QYeKZGX6T-sM';
+>>>>>>> Stashed changes
   }
 }
