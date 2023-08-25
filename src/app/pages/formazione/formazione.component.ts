d082869b7a1d9b31bcb3247ccdbf709b049641d9
Yanina Agüero <yaninaaguero@hotmail.it>
2023-08-25 13:08:44 +0200

    Esperienze e formazione css

--- src/app/pages/formazione/formazione.component.ts
+++ src/app/pages/formazione/formazione.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-formazione',
   templateUrl: './formazione.component.html',
-  styleUrls: ['./formazione.component.scss']
+  styleUrls: ['./formazione.component.scss'],
 })
 export class FormazioneComponent {
-
+  constructor(private router: Router) {}
 }
