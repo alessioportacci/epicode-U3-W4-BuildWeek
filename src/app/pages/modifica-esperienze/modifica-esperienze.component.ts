fc884bf0d08e5703662478e41fa8f0a9695ae39c
maicol abbiento <128224384+MaicolAbbiento@users.noreply.github.com>
2023-08-25 10:28:43 +0200

    ok

--- src/app/pages/modifica-esperienze/modifica-esperienze.component.ts
+++ src/app/pages/modifica-esperienze/modifica-esperienze.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import {
+  IUpdateExperience,
+  Iexperiences,
+} from 'src/app/interfaces/iexperiences';
+import { IProfile } from 'src/app/interfaces/iprofile';
+import { ExperienceService } from 'src/app/services/experience.service';
+import { StriveApiService } from 'src/app/services/strive-api.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-modifica-esperienze',
@@ -8,17 +16,46 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ModificaEsperienzeComponent implements OnInit {
   id!: string;
-  constructor(private route: ActivatedRoute, private router: Router) {
-    this.ngOnInit();
-    this.card();
-  }
-  card() {
-    console.log('c');
+  profileData?: IProfile;
+  experienceData: IUpdateExperience = {
+    role: '',
+    company: '',
+    startDate: '',
+    endDate: '', // could be null
+    description: '',
+    area: '',
+  };
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public experienceSrv: ExperienceService,
+    public striveSrv: StriveApiService
+  ) {}
+  deleteExperience() {
+    this.experienceSrv.removeExperience(this.id).subscribe((res) => {
+      console.log('cancellato');
+      this.experienceSrv.getExperiences().subscribe((data) => {});
+    });
   }
   ngOnInit() {
-    console.log('c');
-
     this.route.paramMap.subscribe((params) => (this.id = params.get('id')!));
-    console.log(this.id);
+    this.striveSrv.getProfile().subscribe((data) => {
+      this.profileData = data;
+      this.experienceSrv.setUserId(data._id);
+
+      this.experienceSrv.getExperiences().subscribe((data) => {
+        this.experienceData = data[0];
+        console.log(this.experienceData.startDate);
+      });
+    });
+  }
+  onSubmit(form: NgForm) {
+    if (form && form.valid) {
+      console.log(this.experienceData);
+
+      this.experienceSrv
+        .updateExperience(this.id, this.experienceData)
+        .subscribe((data) => console.log(data));
+    }
   }
 }
